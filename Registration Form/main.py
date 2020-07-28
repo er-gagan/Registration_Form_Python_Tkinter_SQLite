45e512991e9660f0fafedd745a572bed724115eb
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-07-29 03:40:02 +0530

    Frontend complete
    Frontend complete

--- Registration Form/main.py
+++ Registration Form/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 root = Tk()
-root.geometry("1100x700")
+root.geometry("1100x650")
 root.resizable(0,0)
 
 photo = PhotoImage(file = "./logo.png")
@@ -8,40 +8,65 @@ root.iconphoto(False,photo)
 
 root.title("Registration Form")
 
-state=StringVar()
+fname = StringVar()
+mname = StringVar()
+lname = StringVar()
+phone1 = StringVar()
+phone2 = StringVar()
+pin = StringVar()
+current_address = StringVar()
+permanent_address = StringVar()
+city = StringVar()
+state = StringVar()
+gender = StringVar()
+age = StringVar()
+email = StringVar()
+# area_of_intrest1 = StringVar()
+# area_of_intrest2 = StringVar()
+# area_of_intrest3 = StringVar()
+# area_of_intrest4 = StringVar()
+# area_of_intrest5 = StringVar()
+# area_of_intrest6 = StringVar()
+# area_of_intrest7 = StringVar()
+# area_of_intrest8 = StringVar()
 
-Label(root,text="Student Registration Form",font=("CalibriBlack",25)).place(x=360,y=0)
+gender.set(1)
+
+def Submit():
+    print(fname.get(),mname.get(),lname.get(),phone1.get(),phone2.get(),pin.get(),current_address.get(),permanent_address.get(),city.get(),state.get(),gender.get(),age.get(),email.get(),textarea.get('1.0',END),sep=" ")
+
+Label(root,text="Student Registration Form",font=("CalibriBlack",25)).place(x=360,y=10)
 
 Label(root,text="First Name:",font=("CalibriBlack",15)).place(x=20,y=70)
-e1 = Entry(root,font=("CalibriBlack",15))
+e1 = Entry(root,textvariable=fname,font=("CalibriBlack",15))
 e1.focus()
 e1.place(x=130,y=72)
 
 Label(root,text="Middle Name:",font=("CalibriBlack",15)).place(x=370,y=70)
-Entry(root,font=("CalibriBlack",15)).place(x=500,y=72)
+Entry(root,textvariable=mname,font=("CalibriBlack",15)).place(x=500,y=72)
 
 Label(root,text="Last Name:",font=("CalibriBlack",15)).place(x=740,y=70)
-Entry(root,font=("CalibriBlack",15)).place(x=850,y=72)
+Entry(root,textvariable=lname,font=("CalibriBlack",15)).place(x=850,y=72)
 
 Label(root,text="Phone1:",font=("CalibriBlack",15)).place(x=20,y=110)
-Entry(root,font=("CalibriBlack",15)).place(x=130,y=112)
+Entry(root,textvariable=phone1,font=("CalibriBlack",15)).place(x=130,y=112)
 
 Label(root,text="Phone2:",font=("CalibriBlack",15)).place(x=370,y=110)
-Entry(root,font=("CalibriBlack",15)).place(x=500,y=112)
+Entry(root,textvariable=phone2,font=("CalibriBlack",15)).place(x=500,y=112)
 
 Label(root,text="ZIP/PIN:",font=("CalibriBlack",15)).place(x=740,y=110)
-Entry(root,font=("CalibriBlack",15)).place(x=850,y=112)
+Entry(root,textvariable=pin,font=("CalibriBlack",15)).place(x=850,y=112)
 
 Label(root,text="Current Address:",font=("CalibriBlack",15)).place(x=20,y=150)
-Entry(root,font=("CalibriBlack",15),width=75).place(x=230,y=150)
+Entry(root,textvariable=current_address,font=("CalibriBlack",15),width=75).place(x=230,y=150)
 
-Checkbutton(root,text="If your current address is same as permanent address then check it else not",font=("CalibriBlack",15)).place(x=20,y=190)
+Checkbutton(root,text="If your current address is same as permanent address then check it else not",font=("CalibriBlack",14)).place(x=20,y=185)
 
 Label(root,text="Permanent Address:",font=("CalibriBlack",15)).place(x=20,y=230)
-Entry(root,font=("CalibriBlack",15),width=75).place(x=230,y=230)
+Entry(root,textvariable=permanent_address,font=("CalibriBlack",15),width=75).place(x=230,y=230)
 
 Label(root,text="City:",font=("CalibriBlack",15)).place(x=20,y=270)
-Entry(root,font=("CalibriBlack",15),width=30).place(x=130,y=270)
+Entry(root,textvariable=city,font=("CalibriBlack",15),width=30).place(x=130,y=270)
 
 Label(root,text="State:",font=("CalibriBlack",15)).place(x=500,y=270)
 state_list=["Uttar Pradesh","Uttrakhand","Maharastra","Jammu & Kashmir"]
@@ -52,12 +77,38 @@ o1['menu'].configure(font=("CalibriBlack",15))
 o1.place(x=600,y=268)
 
 Label(root,text="Gender:",font=("CalibriBlack",15)).place(x=20,y=310)
+Radiobutton(root,text="Male",variable=gender,value="Male",font=("CalibriBlack",15)).place(x=150,y=310)
+Radiobutton(root,text="Female",variable=gender,value="Female",font=("CalibriBlack",15)).place(x=250,y=310)
+Radiobutton(root,text="Other",variable=gender,value="Others",font=("CalibriBlack",15)).place(x=350,y=310)
+
 Label(root,text="Area of Intrest:",font=("CalibriBlack",15)).place(x=20,y=350)
+# variable=area_of_intrest1,
+# variable=area_of_intrest2,
+# variable=area_of_intrest3,
+# variable=area_of_intrest4,
+# variable=area_of_intrest5,
+# variable=area_of_intrest6,
+# variable=area_of_intrest7,
+# variable=area_of_intrest8,
+Checkbutton(root,text="Programming",font=("CalibriBlack",15)).place(x=200,y=350)
+Checkbutton(root,text="Cricket",font=("CalibriBlack",15)).place(x=400,y=350)
+Checkbutton(root,text="Tennnis",font=("CalibriBlack",15)).place(x=600,y=350)
+Checkbutton(root,text="Carrom",font=("CalibriBlack",15)).place(x=800,y=350)
+Checkbutton(root,text="Studying",font=("CalibriBlack",15)).place(x=200,y=390)
+Checkbutton(root,text="Reading",font=("CalibriBlack",15)).place(x=400,y=390)
+Checkbutton(root,text="Dancing",font=("CalibriBlack",15)).place(x=600,y=390)
+Checkbutton(root,text="Travelling",font=("CalibriBlack",15)).place(x=800,y=390)
 
+Label(root,text="Age:",font=("CalibriBlack",15)).place(x=20,y=435)
+Entry(root,textvariable=age,font=("CalibriBlack",15)).place(x=100,y=435)
 
-# image1= PhotoImage(file="./back.png")
-# label_for_image= Label(root, image=image1)
-# label_for_image.place(x=-2, y=-1)
+Label(root,text="Email:",font=("CalibriBlack",15)).place(x=400,y=435)
+Entry(root,textvariable=email,font=("CalibriBlack",15),width=45).place(x=500,y=435)
 
+Label(root,text="Any Suggession:",font=("CalibriBlack",13)).place(x=20,y=475)
+textarea = Text(root,font=("CalibriBlack",15),bd=5,height=5)
+textarea.place(x=20,y=500)
 
+Button(root,text="Submit",font=("CalibriBlack",20),bd=8,command=Submit).place(x=940,y=500)
+Button(root,text="Clear",font=("CalibriBlack",16),bd=5,width=9).place(x=940,y=575)
 root.mainloop()
--- Registration Form/tempCodeRunnerFile.py
+++ Registration Form/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-.png")
-# label_for_image= Label(root, image=image1)
-# label_for_image.place(x=-2, y=-
+1
